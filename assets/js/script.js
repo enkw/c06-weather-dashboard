9320c2e34ab5496b5e42741e2e7300512dc6215e
enkw <iankwolfe@gmail.com>
2024-03-13 02:39:41 -0400

    Added js for the 5 day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,7 +9,7 @@ function getWeather (event) {
     // event.preventDefault();
 
     const userInput = userInputEl.val();
-    const requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${key}&units=imperial`;
+    const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=${key}&units=imperial`;
 
     console.log(userInput);
     console.log(requestUrl);
@@ -27,18 +27,40 @@ function getWeather (event) {
 
 // This function prints the results fetched above and prints them to a page as a card
 function printResults(data) {
-    const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    console.log(data);
+    // Reference for the icon
+    const iconUrl = `https://openweathermap.org/img/wn/${data.list[0].weather[0].icon}.png`
+    // Today's weather card section
     const resultsCard = `
     <div class="card">
         <div class="card-body">
-            <h3 class="card-text">${data.name}</h3>
+            <h3 class="card-text">${data.city.name}</h3>
             <img src="${iconUrl}" alt="Weather Condition Icon">
-            <p class="card-text">Temp: ${data.main.temp}°F</p>
-            <p class="card-text">Wind: ${data.wind.speed} mph</p>
-            <p class="card-text">Humidity: ${data.main.humidity}</p>
+            <p class="card-text">Temp: ${data.list[0].main.temp}°F</p>
+            <p class="card-text">Wind: ${data.list[0].wind.speed} mph</p>
+            <p class="card-text">Humidity: ${data.list[0].main.humidity}</p>
         </div>
     </div>`;
     $('#local-weather').html(resultsCard);
+    // Five day forecast section
+    let forecastCards = `<div class="card-group">`
+    // For loop to get every eigh results (every 24 hours) and print them to a card group
+    for (let i = 0; i < 5; i++) {
+        const index = i * 8;
+        const forecast = data.list[index];
+        forecastCards += `
+        <div class="card">
+            <div class="card-body">
+                <h3 class="card-title">ADD TIME AND UPDATE ICON</h3>
+                <img src="${iconUrl}" alt="Weather Condition Icon"></img>
+                <p class="card-text">Temp: ${forecast.main.temp}°F</p>
+                <p class="card-text">Wind: ${forecast.wind.speed} mph</p>
+                <p class="card-text">Humidity: ${forecast.main.humidity}</p>
+            </div>
+        </div>
+    </div>`;
+    }
+    $('#forecast').html(forecastCards);
 };
 
 // Causes getWeather to run when the user clicks the "Search" button
