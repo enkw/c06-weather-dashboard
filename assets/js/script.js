c478058d60944d55857c921eb421d79de39234a5
enkw <iankwolfe@gmail.com>
2024-03-13 01:38:15 -0400

    JS now prints fetch results to the html, started basic css adjustments to bootstrap added

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,24 +3,43 @@ const userInputEl = $('#user-input');
 const btnEl = $('#btn');
 const recentSearch = "";
 
+// This function grabs the weather for the city entered by the user
 function getWeather (event) {
-    event.preventDefault();
+// Look into why preventDefault throws errors when enabled
+    // event.preventDefault();
 
     const userInput = userInputEl.val();
-    const requestUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${userInput}&appid=${key}`;
+    const requestUrl = `https://api.openweathermap.org/data/2.5/weather?q=${userInput}&appid=${key}&units=imperial`;
 
     console.log(userInput);
     console.log(requestUrl);
 
-    fetch(requestUrl).then(function (response) {
-        if (response.ok) {
-            response.json().then(function (data) {
-                console.log(data);
-            })
-        } else {
-            console.log("that didn't work dawg");
+    fetch(requestUrl).then(response => {
+        if (!response.ok) {
+            throw Error('Network response not found');
         }
-    })
-}
+        return response.json();
+    }).then(data => printResults(data))
+    .catch(error => {
+        console.error('Error', error);
+    });
+};
 
+// This function prints the results fetched above and prints them to a page as a card
+function printResults(data) {
+    const iconUrl = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`
+    const resultsCard = `
+    <div class="card">
+        <div class="card-body">
+            <h3 class="card-text">${data.name}</h3>
+            <img src="${iconUrl}" alt="Weather Condition Icon">
+            <p class="card-text">Temp: ${data.main.temp}°F</p>
+            <p class="card-text">Wind: ${data.wind.speed} mph</p>
+            <p class="card-text">Humidity: ${data.main.humidity}</p>
+        </div>
+    </div>`;
+    $('#local-weather').html(resultsCard);
+};
+
+// Causes getWeather to run when the user clicks the "Search" button
 btnEl.on('click', getWeather);
